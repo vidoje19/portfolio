d3457c97cab3e9025a22f8c0bcb02f192fe22c09
Vidoje <andjelicvidoje5@gmail.com>
2022-07-31 19:10:52 +0200

    Add files via upload

--- Website/js.js
+++ Website/js.js
@@ -1,9 +1,22 @@
-$(document).ready(function()){
-    $(window).scroll(function()){
-        if(this.scrollY > 20){
-            $('')
-        }else {
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    }else {
+      $('.navbar').removeClass("sticky")
+    }
+  });
 
-        }
-    })
+  const hamburger = document.querySelector(".hamburger")
+  const menu = document.querySelector(".menu")
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".navlink").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    menu.classList.remove("active");
+  }))
 });
